98ecd64bbcae8af6d8768718584e9ac959ba423c
Kevin Gabinete <kgabinete08@gmail.com>
2015-04-15 19:18:50 -0700

    implementing admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, length: {minimum: 5}
+  #validates :password, presence: true, length: {minimum: 5}
 
   sluggable_column :username
 
